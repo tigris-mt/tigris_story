1e9e9d640c06e7f043ce77807f74e572a907485f
Beha <shacknetisp@mail.com>
2018-10-20 11:17:27 -0400

    Story-based home and recall functions.

--- prayer.lua
+++ prayer.lua
@@ -1,31 +1,93 @@
+local function prayer_function(id, label, icon, mana, delay, func)
+    local ename = "tigris_story:effect_" .. id
+
+    local players = {}
+
+    local function alert(player, message)
+        minetest.chat_send_player(player:get_player_name(), "[" .. label .. "] " .. message)
+    end
+
+    playereffects.register_effect_type(ename, label, icon, {ename}, function(player)
+        local state = players[player:get_player_name()]
+        if not state then
+            playereffects.cancel_effect_type(ename, true, player:get_player_name())
+            return false
+        end
+
+        if player:get_hp() ~= state.hp or vector.distance(state.pos, player:getpos()) > 1 then
+            alert(player, "Your body was disrupted.")
+            playereffects.cancel_effect_type(ename, true, player:get_player_name())
+            return false
+        end
+
+        state.times = state.times + 1
+        if state.times >= delay then
+            func(player, function(message)
+                alert(player, message)
+            end)
+            return false
+        end
+    end,
+    function(effect, player)
+    end, false, true, 1)
+
+    minetest.register_on_leaveplayer(function(player)
+        playereffects.cancel_effect_type(ename, true, player:get_player_name())
+    end)
+
+    return (function(name)
+        local player = minetest.get_player_by_name(name)
+        if not player then
+            return
+        end
+        if tigris.magic.mana(player) < mana then
+            alert(player, "Your mana is too weak.")
+            return
+        end
+        tigris.magic.mana(player, -mana, true)
+
+        players[name] = {
+            pos = player:getpos(),
+            hp = player:get_hp(),
+            times = 0,
+        }
+
+        playereffects.apply_effect_type(ename, delay, player)
+    end)
+end
+
 local prayers = {
     recall = {
         description = "Call upon Inemyde for transportation to the Room of Light.",
-        func = function(name)
-            local player = minetest.get_player_by_name(name)
-            if not player then
-                return
-            end
-            local pos, hp = player:getpos(), player:get_hp()
-            minetest.chat_send_player(name, "Your prayer has been heard. Remain stationary and calm while power gathers.")
-            tigris.magic.mana(player, -25, true)
-            minetest.after(5, function()
-                local player = minetest.get_player_by_name(name)
-                if not player or player:get_hp() ~= hp or vector.distance(pos, player:getpos()) > 1 then
-                    minetest.chat_send_player(name, "Your body was disturbed and unable to teleport.")
-                    return
-                end
-                minetest.chat_send_player(name, "The warmth of Inemyde envelops you.")
-                player:setpos(({underground_start.box()})[1])
-                player:set_look_horizontal(0)
-            end)
-        end,
+        func = prayer_function("recall", "Recall", "tigris_story_prayer_recall.png", 10, 5, function(player)
+            player:setpos(({underground_start.box()})[1])
+            player:set_look_horizontal(0)
+        end)
     },
 }
 
 for k,v in pairs(prayers) do
-    minetest.register_chatcommand("p" .. k, {
+    minetest.register_chatcommand(k, {
         description = v.description,
         func = v.func,
     })
+end
+
+-- Override home function.
+local old = minetest.registered_chatcommands["home"].func
+minetest.override_chatcommand("home", {
+    description = "Channel the power of Inemyde and warp to your home location.",
+    privs = {home = true},
+    func = prayer_function("home", "Home", "tigris_story_prayer_home.png", 10, 7, function(player, alert)
+        local _, message = old(player:get_player_name())
+        alert(message)
+    end)
+})
+
+if minetest.settings:get_bool("tigris.story.force_home_priv", true) then
+    minetest.register_on_newplayer(function(player)
+        local privs = minetest.get_player_privs(player:get_player_name())
+        privs.home = true
+        minetest.set_player_privs(player:get_player_name(), privs)
+    end)
 end
